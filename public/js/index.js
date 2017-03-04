14a5387d2d61706e7639df29c641c4702d8e72cc
andrianb <andrianb@users.noreply.github.com>
2017-03-04 19:47:44 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,24 +30,32 @@ socket.on('newLocationMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
+
+    var messageTextBox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
-    }, function() {});
+        text: $(messageTextBox).val()
+    }, function() {
+        $(messageTextBox).val('');
+    });
 })
 
-var locationButton = $('#send-location');
+var locationButton = jQuery('#send-location');
 locationButton.on('click', function() {
     if (!navigator.geolocation) {
-        return alert('Geolocation not suported by your browser.');
+        return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
-        alert('Unable to fetch location.')
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.');
     });
 });
