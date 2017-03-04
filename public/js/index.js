a2921b22ebb6803a1dc2a469beaa7832a4c3f56a
andrianb <andrianb@users.noreply.github.com>
2017-03-04 16:37:26 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('con to server');
 
-    socket.emit('createMessage', {
-        to: 'mike@exemple.com',
-        text: 'hey. how are you'
-    });
 });
 
 socket.on('disconnect', function() {
