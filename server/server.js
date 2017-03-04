5fe197872083cfa06ac7c91a9829a01fc7f45b27
andrianb <andrianb@users.noreply.github.com>
2017-03-04 16:47:11 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -21,6 +21,18 @@ io.on('connection', (socket) => {
         createAt: 123
     });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcom to the chat',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -28,6 +40,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createAt: new Date().getTime()
         });
+    /*        socket.broadcast.emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createAt: new Date().getTime()
+            });*/
     });
 
     socket.on('disconnect', () => {
